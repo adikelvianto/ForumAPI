fb726e60f95ad3876ee024afbbab1e9e5d4345f6
Adi Kelvianto <adikelvianto2906@gmail.com>
2023-07-19 15:54:24 +0700

    thread repository done

---
+++ src/Domains/threads/ThreadRepository.js
@@ -0,0 +1,11 @@
+class ThreadRepository {
+  async addThread(newThread) {
+    throw new Error('THREAD_REPOSITORY.ADD_METHOD_NOT_IMPLEMENTED');
+  }
+  
+  async verifyAvailableThread(threadId) {
+    throw new Error('THREAD_REPOSITORY.VERIFY_METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = ThreadRepository;
---
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -0,0 +1,12 @@
+const ThreadRepository = require('../ThreadRepository');
+
+describe('ThreadRepository interface', () => {
+  it('should throw an error when calling an abstract behavior', async () => {
+    // Arrange
+    const threadRepository = new ThreadRepository();
+
+    // Action and Assert
+    await expect(threadRepository.addThread({})).rejects.toThrowError('THREAD_REPOSITORY.ADD_METHOD_NOT_IMPLEMENTED');
+    await expect(threadRepository.verifyAvailableThread('')).rejects.toThrowError('THREAD_REPOSITORY.VERIFY_METHOD_NOT_IMPLEMENTED');
+  });
+});
