05c172e4d95ffafec7aa1a3d8a235f406f3ed784
Adi Kelvianto <adikelvianto2906@gmail.com>
2023-07-19 16:08:40 +0700

    add thread use case done

---
+++ src/Applications/use_case/_test/AddThreadUseCase.test.js
@@ -0,0 +1,53 @@
+const NewThread = require('../../../Domains/threads/entities/NewThread');
+const AddedThread = require('../../../Domains/threads/entities/AddedThread');
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
+const AddThreadUseCase = require('../AddThreadUseCase');
+
+describe('AddThreadUseCase', () => {
+
+  it('should correctly orchestrate the add thread action', async () => {
+    // Arrange
+    const useCasePayload = {
+      title: 'A new thread',
+      body: 'The body of a new thread',
+      owner: 'user-abc',
+    };
+
+    const mockAddedThread = new AddedThread({
+      id: 'thread-123',
+      title: useCasePayload.title,
+      body: useCasePayload.body,
+      date: '2023-7-18T16:00:00.000Z',
+      owner: useCasePayload.owner,
+    });
+
+    // Create the mock thread repository
+    const mockThreadRepository = new ThreadRepository();
+
+    // Mock the "addThread" function
+    mockThreadRepository.addThread = jest.fn().mockImplementation(() => Promise.resolve(
+      new AddedThread({
+        id: 'thread-123',
+        title: useCasePayload.title,
+        body: useCasePayload.body,
+        date: '2023-7-18T16:00:00.000Z',
+        owner: useCasePayload.owner,
+      }),
+    ));
+
+    const addThreadUseCase = new AddThreadUseCase({
+      threadRepository: mockThreadRepository,
+    });
+
+    // Action
+    const addedThread = await addThreadUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(addedThread).toStrictEqual(mockAddedThread);
+    expect(mockThreadRepository.addThread).toBeCalledWith(new NewThread({
+      title: useCasePayload.title,
+      body: useCasePayload.body,
+      owner: useCasePayload.owner,
+    }));
+  });
+});
