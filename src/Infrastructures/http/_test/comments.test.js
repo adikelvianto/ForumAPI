5e5f346c88fbd8537c9ad5ad1fd2b49cda31eac0
Adi Kelvianto <adikelvianto2906@gmail.com>
2023-08-05 19:34:37 +0700

    add timeout

--- src/Infrastructures/http/_test/comments.test.js
+++ src/Infrastructures/http/_test/comments.test.js
@@ -149,14 +149,14 @@ describe('/comments endpoint', () => {
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
-      }, 30000);
+      });
 
       // assert
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(404);
       expect(responseJson.status).toEqual('fail');
       expect(responseJson.message).toEqual('Thread not found');
-      });
+    }, 30000);
 
   });
 
